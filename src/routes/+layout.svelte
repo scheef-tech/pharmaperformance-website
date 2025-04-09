e600365fba684cad396f7540677a5ae72cc0935e
Nik Divjak <divjak.nik@gmail.com>
2025-04-09 14:15:01 +0200

    Added language

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,15 @@
 <script lang="ts">
 	import '../app.css';
+	import { locales, localizeHref } from '$lib/paraglide/runtime';
+	import { page } from '$app/state';
 
 	let { children } = $props();
 </script>
 
 {@render children()}
+
+<div style="display:none">
+	{#each locales as locale}
+		<a href={localizeHref(page.url.pathname, { locale })}>{locale}</a>
+	{/each}
+</div>
