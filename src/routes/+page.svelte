1226897e48ba3eddb6649fe0d23092b9860e99f2
Nik Divjak <divjak.nik@gmail.com>
2025-04-27 19:07:54 +0200

    Refines meta tags and enhances announcement styling
    Consolidates meta tag formatting for improved readability.
    Enhances the announcement section with additional styling
    to improve visibility and user engagement.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,36 +9,20 @@
 
 <svelte:head>
 	<title>{m.stout_clean_pig_embrace()}</title>
-	<meta
-		name="description"
-		content={m.extra_pink_niklas_forgive()}
-	/>
-	<meta
-		name="keywords"
-		content={m.trick_sour_deer_rise()}
-	/>
-	<meta
-		property="og:title"
-		content={m.actual_glad_swan_quiz()}
-	/>
-	<meta
-		property="og:description"
-		content={m.loose_antsy_orangutan_dash()}
-	/>
+	<meta name="description" content={m.extra_pink_niklas_forgive()} />
+	<meta name="keywords" content={m.trick_sour_deer_rise()} />
+	<meta property="og:title" content={m.actual_glad_swan_quiz()} />
+	<meta property="og:description" content={m.loose_antsy_orangutan_dash()} />
 	<meta property="og:type" content="website" />
 	<meta property="og:url" content="https://www.pharmaperformance.de/" />
 	<meta name="twitter:card" content="summary_large_image" />
-	<meta
-		name="twitter:title"
-		content={m.plane_tired_gazelle_climb()}
-	/>
-	<meta
-		name="twitter:description"
-		content={m.sharp_sound_pony_fry()}
-	/>
+	<meta name="twitter:title" content={m.plane_tired_gazelle_climb()} />
+	<meta name="twitter:description" content={m.sharp_sound_pony_fry()} />
 </svelte:head>
 
-<div class="w-full text-center">PHARMA PERFORMANCE coming soon!</div>
+<div class="mb-3 w-full text-center text-xl font-normal">
+	<strong> PHARMA PERFORMANCE coming soon! </strong>
+</div>
 
 <div class="flex w-full justify-center">
 	<HeroItemCarousel items={data.heroItemCarousel} />
